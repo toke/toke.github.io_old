f3f942da47751e0dac8974c82e9205552d50aadf
Thomas Kerpe <toke@toke.de>
2011-08-25 18:05:47 +0200

    Added local serve command in fabfile

--- fabfile.py
+++ fabfile.py
@@ -2,6 +2,8 @@ from fabric.api import *
 import os
 import fabric.contrib.project as project
 
+def serve():
+    local('jekyll --url http://127.0.0.1:4000 --server')
 
 def gitpush():
     local('git push')
